084011f9d63c7a531002c0a2f88a4fe437714947
VOID20030823 <154111368+VOID20030823@users.noreply.github.com>
2023-12-25 10:49:55 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://void20030823.github.io/FROG11028002/model.json';
 
 
 function preload() {
